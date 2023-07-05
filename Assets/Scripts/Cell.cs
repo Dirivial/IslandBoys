2c4db5e089a5d21da03aef496070a446a8558195
Dirivial <alexander.kadeby@gmail.com>
2023-07-05 21:00:02 +0200

    Added height to cells

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,8 +1,14 @@
 
 public class Cell {
     public bool isWater;
+    public int height;
 
     public Cell(bool isWater) {
         this.isWater = isWater;
+    }
+
+    public Cell(bool isWater, int height) {
+        this.isWater = isWater;
+        this.height = height;
     }
 }
--- Assets/Scripts/Island.cs
+++ Assets/Scripts/Island.cs
@@ -14,11 +14,57 @@ public class Island : MonoBehaviour
     private int sizeY = 0;
     private GridManager grid;
 
+    private int largestDistance = 0;
+    private float waterBuffer = 20f;
+
     void Start() {
 
         sizeX = GridManager.Instance.gridSizeX;
         sizeY = GridManager.Instance.gridSizeY;
 
+        Vector3 centerPosition = new Vector3(sizeX, 0f, sizeY) / 2f;
+
+        int[,] heightMap = new int[sizeX, sizeY];
+
+        for(int y = 0; y < sizeY; y++) {
+            for(int x = 0; x < sizeX; x++) {
+                if (x > waterBuffer && x < sizeX - waterBuffer && y > waterBuffer && y < sizeY - waterBuffer) {
+                    // Calculate the distance from the center for each cell
+                    Vector3 cellPosition = new Vector3(x, 0f, y);
+                    float distance = Vector3.Distance(cellPosition, centerPosition) / 15f;
+
+                    // Set the value of the cell based on the distance from the center
+                    // You can modify this calculation to suit your needs
+                    int cellValue = Mathf.RoundToInt(distance);
+                    heightMap[x, y] = cellValue;
+
+                    if (cellValue > largestDistance) {
+                        largestDistance = cellValue;
+                    }
+                } else {
+                    heightMap[x, y] = -1;
+                }
+            }
+        }
+
+        grid = GridManager.Instance;
+        for(int y = 0; y < sizeY; y++) {
+            for(int x = 0; x < sizeX; x++) {
+                int height = heightMap[x, y];
+                if (height < 0) {
+                    grid.SetGridCell(x, y, new Cell(true, largestDistance - height));
+                } else {
+                    grid.SetGridCell(x, y, new Cell(false, largestDistance - height));
+                    GameObject ground = Instantiate(groundPrefab, new Vector3(x * 2, (largestDistance - heightMap[x, y]) * GridManager.Instance.cellSize, y * 2), Quaternion.identity);
+                    ground.transform.parent = transform;
+                }
+            }
+        }
+    }
+
+
+    // This code is used to generate the noise map and falloff map
+    private void funkyStuff() {
         float[,] noiseMap = new float[sizeX, sizeY];
 
         (float xOffset, float yOffset) = (Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
